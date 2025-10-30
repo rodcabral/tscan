7041d1e41ba18c0fa031e8fb4d78463fb1744f62
Rodrigo Cabral <37848619+rodcabral@users.noreply.github.com>
2025-10-30 20:48:31 -0300

    load logo

--- src/main.c
+++ src/main.c
@@ -1,7 +1,23 @@
 #include "../include/tscan.h"
 
+void load_logo() {
+    FILE* fptr = fopen("/usr/share/tscan/tscan-logo", "r");
+
+    printf("\n");
+
+    char c;
+    while((c = fgetc(fptr)) != EOF) {
+        printf("%c", c);
+    }
+
+    printf("\nVersion 1.0 (BETA)\n\n");
+
+    fclose(fptr);
+}
+
 int main(int argc, char** argv) {
     if(argc < 2) {
+        load_logo();
         fprintf(stderr, "USAGE: tscan [target]\n");
         return -1;
     }
